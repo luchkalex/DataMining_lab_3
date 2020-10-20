d86249356a7c2d28da24e99ad51bd3a7701f7596
luchkalex <tedy5ruzvelt@gmail.com>
2020-10-20 20:17:11 +0300

    Feat: Entry point updated

--- main.py
+++ main.py
@@ -1,21 +1,3 @@
-from tkinter import *
-import matplotlib.pyplot as plt
-import numpy as np
+from ui.main_form import show_main_form
 
-
-def graph():
-    prices = np.random.normal(20000, 1000, 5000)
-    plt.hist(prices, 50)
-    plt.show()
-
-
-root = Tk()
-root.title = "Title"
-mainLabel = Label(root, text="MyLabel", font=("Times New Roman", 14, 'bold'))
-mainLabel.pack()
-graphButton = Button(root, text="Show", command=graph)
-graphButton.pack()
-root.mainloop()
-
-
-
+show_main_form()
