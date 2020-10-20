6e26c0a914ae8bfa92683e0e7667d20fa298b34d
luchkalex <tedy5ruzvelt@gmail.com>
2020-10-20 20:14:19 +0300

    Color constants defining

--- constants.py
+++ constants.py
@@ -3,6 +3,23 @@ SOURCE_FILENAME_2 = "birch2.txt"
 SOURCE_FILENAME_3 = "birch3.txt"
 SOURCE_FILENAME_4 = "s1.txt"
 
+file_name_cases = {
+    1: SOURCE_FILENAME_1,
+    2: SOURCE_FILENAME_2,
+    3: SOURCE_FILENAME_3,
+    4: SOURCE_FILENAME_4,
+}
+
 OUTPUT_DIR = "output"
 SOURCE_DIR = "source_files"
+
+COLORS = ["#9b59b6", "#ff5733",
+          "#2980b9", "#1abc9c",
+          "#1228f3", "#212f3c",
+          "#909497", "#a04000",
+          "#f39c12", "#f1c40f",
+
+          "#f312ec", "#f3126e",
+          "#5c12f3", "#bd9356",
+          "#862669"]
 
