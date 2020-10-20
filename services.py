2862ecf924c8b9eec32b4b9e428f6f5dc12f4ff7
luchkalex <tedy5ruzvelt@gmail.com>
2020-10-20 20:15:34 +0300

    Feat: Points parsing from string

--- services.py
+++ services.py
@@ -1,10 +1,9 @@
 from re import *
-
-from constants import OUTPUT_DIR
+from models.Point import Point
 
 
 def get_list_of_lines_from_file(src_filename):
-    src_file = open(OUTPUT_DIR + "/" + src_filename, "r")
+    src_file = open(src_filename, "r")
     result_list = []
 
     for line in src_file:
@@ -12,3 +11,19 @@ def get_list_of_lines_from_file(src_filename):
 
     return result_list
 
+
+def get_point_from_string(src_str):
+    return Point(
+        int(sub("\\d+\\s*$", "", src_str)),
+        int(sub("^\\s+\\d+", "", src_str)),
+        -1
+    )
+
+
+def get_points_from_string_list(src_list):
+    points = []
+
+    for line in src_list:
+        points.append(get_point_from_string(line))
+
+    return points
